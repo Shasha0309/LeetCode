fda4981b9e45d9da83b9ff785e5140422cf84ebc
Shasha0309 <sona2002sharma@gmail.com>
2024-05-30 03:13:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (61.08%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix[0].size();j++){
+                
+                swap(matrix[i][j],matrix[j][i]);
+                
+            }
+        }
+
+        int left = 0;
+        int right = matrix[0].size()-1;
+        while(left<right){
+        for(int i=0;i<matrix.size();i++){
+            
+            swap(matrix[i][left],matrix[i][right]);
+        }
+            left++;
+            right--;
+        
+        }
+    } 
+};
