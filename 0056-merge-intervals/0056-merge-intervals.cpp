455ff6dacf361c61a8253176455d0a1cd81d15e4
Shasha0309 <sona2002sharma@gmail.com>
2024-06-12 20:54:42 +0530

    Time: 13 ms (98.16%), Space: 22.7 MB (68.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-       sort(intervals.begin(),intervals.end());
-       vector<vector<int>> ans;
-       ans.push_back(intervals[0]);
-       for(int i=1;i<intervals.size();i++){
-        if(ans.back()[1]>=intervals[i][0]){
-            if(ans.back()[1]>intervals[i][1]) continue;
-            ans.back()[1] = intervals[i][1];
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(ans.back()[1]>=intervals[i][0]){
+                if(ans.back()[1]>intervals[i][1]) continue;
+                    ans.back()[1]=intervals[i][1];
+                
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
         }
-        else{
-            ans.push_back(intervals[i]);
-        }
-       }
         return ans;
-        }
+    }
 };
