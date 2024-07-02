a6afd717faf24f013236547ac8647819c5c89dcb
Shasha0309 <sona2002sharma@gmail.com>
2024-07-02 20:13:35 +0530

    Time: 13 ms (30.67%), Space: 9.5 MB (9.5%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -12,12 +12,9 @@ public:
         while(r<s.size()){
                 hash[s[r]-'A']++;
                 maxf=max(maxf,hash[s[r]-'A']);
-                while(((r-l+1)-maxf)>k){
+                if(((r-l+1)-maxf)>k){
                     hash[s[l]-'A']--;
-                    maxf=0;
-                    for(int i=0;i<26;i++){
-                        maxf=max(maxf,hash[i]);
-                    }
+                    //maxf=0;
                     l++; 
                 }
                maxlen=max(maxlen,r-l+1);
