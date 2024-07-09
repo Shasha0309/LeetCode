2a780fa655aa0f8fba3001795fcb891541a051c8
Shasha0309 <sona2002sharma@gmail.com>
2024-07-09 20:19:24 +0530

    Time: 24 ms (51.25%), Space: 22.5 MB (96.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,18 +3,17 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        int n = intervals.size();
         ans.push_back(intervals[0]);
+        int n = intervals.size();
         for(int i=1;i<n;i++){
             if(ans.back()[1]>=intervals[i][0]){
                 if(ans.back()[1]>intervals[i][1]) continue;
-                    ans.back()[1]=intervals[i][1];
-                
+                ans.back()[1]=intervals[i][1];
             }
             else{
                 ans.push_back(intervals[i]);
             }
-        }
-        return ans;
+    }
+    return ans;
     }
 };
