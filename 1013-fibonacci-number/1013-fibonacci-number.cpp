e7bd08fbf5342310bb4c1c0e573af37f9b11c4b7
Shasha0309 <sona2002sharma@gmail.com>
2024-07-23 02:54:14 +0530

    Time: 0 ms (100%), Space: 7.4 MB (7.64%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -8,7 +8,15 @@ class Solution {
     }
 public:
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        int prev2 = 0;
+        int prev = 1;
+        if(n==0) return prev2;
+        if(n==1) return 1;
+        for(int i=2;i<=n;i++){
+             int curri=prev2+prev;
+            prev2 = prev;
+            prev=curri;
+        }
+        return prev;
     }
 };
