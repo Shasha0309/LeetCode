e59ddc9f62fae88f90d40535bf1137112e911160
Shasha0309 <sona2002sharma@gmail.com>
2024-08-12 20:58:25 +0530

    Time: 72 ms (21.27%), Space: 16.6 MB (57.33%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    bool solve(string &S1, int i) {
+    // S1 is taken in 1-based indexing
+    for (int j = 1; j <= i; j++) {
+        if (S1[j - 1] != '*') return false;
+    }
+    return true;
+}
+public:
+    bool isMatch(string s, string p) {
+         int n1 = s.size();
+        int n2 = p.size();
+        vector<vector<bool>> dp(n1+1,vector<bool>(n2+1,false));
+        dp[0][0] = true;
+        for(int i=1;i<=n1;i++){
+            dp[i][0] = false;;
+        }
+
+        for(int j=1;j<=n2;j++){
+          dp[0][j]=solve(p,j);
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
