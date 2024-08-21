256245213b7a801e42df050a3a1ff12b62b60a3b
Shasha0309 <sona2002sharma@gmail.com>
2024-08-22 00:04:06 +0530

    Time: 24 ms (12.98%), Space: 21.9 MB (22.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int left = 0;
-        int n = nums.size();
-        for(int right=0;right<n;right++){
-            if(nums[right]!=0){
-                swap(nums[left],nums[right]);
-                left++;
-                
+        if(nums.size()>1){
+        int i=0;
+        int j=1;
+        while(j<nums.size()){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[i]==0 && nums[j]==0){
+                j++;
+            }
+            else{
+                i++;
+                j++;
             }
         }
+    }
     }
 };
