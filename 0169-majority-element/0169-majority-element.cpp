dbc16f519c1a8da9c142caf68ac35db8922359e3
Shasha0309 <sona2002sharma@gmail.com>
2024-08-23 20:38:28 +0530

    Time: 17 ms (58.38%), Space: 27.1 MB (39.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,8 +4,12 @@ public:
         int n = nums.size();
         map<int,int> mpp;
         for(int i=0;i<n;i++){
-            if(mpp[nums[i]]>=n/2){ return nums[i]; }
             mpp[nums[i]] += 1;
+        }
+        for(auto it:mpp){
+            if(it.second>n/2){
+                return it.first;
+            }
         }
         return 0;
     }
