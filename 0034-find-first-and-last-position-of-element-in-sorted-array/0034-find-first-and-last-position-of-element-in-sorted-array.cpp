a97ab7abd525687520b7e827eebfc26cac6cbbd2
Shasha0309 <sona2002sharma@gmail.com>
2024-08-27 16:41:06 +0530

    Time: 3 ms (89.08%), Space: 16.2 MB (63.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,54 +5,34 @@ public:
         int low = 0;
         int high = n-1;
         int mid;
-        int ans1=-1;
-        int ans2=-1;
-         while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (nums[mid] < target) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-            if (nums[mid] == target) {
-                ans1 = mid;
-            }
-        }
-    }
-    
-    // Find the last occurrence of the target
-    low=0;
-    high=n-1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (nums[mid] > target) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-            if (nums[mid] == target) {
-                ans2 = mid;
-            }
-        }
-    }
-        /*if(n==0){
-            return {ans1,ans1};
-        }
-        //vector<int> ans;
+        int ans1 = -1;
+        int ans2 = -1;
         while(low<=high){
             mid = low+(high-low)/2;
             if(nums[mid]<target){
-                low = mid+1;
+                low=mid+1;
             }
-            else if(nums[mid]>target){
+            else{ 
                 high = mid-1;
-                
+                if(nums[mid]==target){
+                ans1 = mid;
+            }
+            }
+        }
+        low = 0;
+         high = n-1;
+         while(low<=high){
+            mid = low+(high-low)/2;
+            if(nums[mid]>target){ 
+                high = mid-1;
             }
             else{
-                ans1=mid;
-                ans2 = mid+1;
-                break;
+                low=mid+1;
+                if(nums[mid]==target){
+                ans2 = mid;
             }
-        }*/
-        //if(ans1!=-1) ans2 = ans1++;
+            }   
+        }
         return {ans1,ans2};
     }
 };
