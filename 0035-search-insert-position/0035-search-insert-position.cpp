7e6bf4247bd092a027d8b16201e9c03bb1c8a2fd
Shasha0309 <sona2002sharma@gmail.com>
2024-08-27 15:02:27 +0530

    Time: 7 ms (23.02%), Space: 12.3 MB (50.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,18 +4,17 @@ public:
         int low = 0;
         int high = nums.size()-1;
         int mid;
+        int ans = nums.size();
         while(low<=high){
             mid = low+(high-low)/2;
             if(nums[mid]<target){
                 low = mid+1;
             }
-            else if(nums[mid]>target){
+            else if(nums[mid]>=target){
+                ans = mid;
                 high = mid-1;
         }
-            else{
-                return mid;
-            }
         }
-        return low;
+        return ans;
     }
 };
