c39da4ea4f76d81f108623bb08e3acf6ff602623
Shasha0309 <sona2002sharma@gmail.com>
2024-08-28 10:33:01 +0530

    Time: 4 ms (40.94%), Space: 14 MB (35.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,21 +8,21 @@ public:
         while(low<=high){
             mid = low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]<nums[low]){
-               if(target>=nums[mid] && target<=nums[high]){
-                low=mid+1;
-               }
-               else{
-                high = mid-1;
-               }
+            if(nums[mid]>=nums[low]){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
             else{
-               if(target<=nums[mid] && target>=nums[low]){
-                high=mid-1;
-               }
-               else{
-                low = mid+1;
-               }
+                if(target>=nums[mid] && target<=nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
