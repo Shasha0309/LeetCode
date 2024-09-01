fdc857b2e268faab3e65a6a599d8b508a7af9fbd
Shasha0309 <sona2002sharma@gmail.com>
2024-09-01 14:32:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,10 +11,10 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL || head->next==NULL) return false;
         ListNode* slow = head;
-        ListNode* fast = head->next;
+        ListNode* fast = head;
         while(slow!=NULL && fast->next!=NULL){
-            slow=slow->next;
             fast=fast->next->next;
+            slow=slow->next;
             if(slow==fast) return true;
         }
         return false;
