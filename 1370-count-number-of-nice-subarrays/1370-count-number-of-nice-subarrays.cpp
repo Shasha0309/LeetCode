a3fa2255718d11977a5a1c849f09e8660bdb0dd3
Shasha0309 <sona2002sharma@gmail.com>
2024-09-09 00:30:01 +0530

    Time: 85 ms (87.5%), Space: 76.4 MB (22.88%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,52 +1,27 @@
 class Solution {
-public:
-int helper(vector<int>nums,int goal){
-    int l=0;
-    int r=0;
-    int cnt = 0;
-    int sum=0;
-    if(goal<0) return 0;
-    while(r<nums.size()){
-        //sum+=nums[r];
-        if(nums[r]%2!=0) sum++;
-        while(sum>goal){
-            //sum=sum-nums[l];
-            if(nums[l]%2!=0)sum--;
-            l++;
-        }
-        cnt+=(r-l+1);
-        r++;
-    }
-    return cnt;
-}
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        /*int l=0;
-        int r=0;
-        map<int,int> mpp;
-        for(int i=0;i<2;i++){
-            mpp[i] = 0;
-        }
+    int solve(vector<int> nums,int k){
+        int l = 0;
+        int r = 0;
         int cnt = 0;
+        int odd = 0;
+        if(k<0) return 0;
         while(r<nums.size()){
-            if(nums[r]%2==0){
-                mpp[1]++;
+            if(nums[r]%2!=0){
+                odd++;
             }
-            else{
-                mpp[0]++;
-            }
-            while(mpp[0]>k && l<=r){
-                cnt++;
-                if(nums[l]%2==0){
-                    mpp[1]--;
-                }
-                else{
-                    mpp[0]--;
+            while(odd>k){
+                if(nums[l]%2!=0){
+                    odd--;
                 }
                 l++;
             }
+            cnt+=(r-l+1);
             r++;
         }
-        return cnt;*/
-        return helper(nums,k)-helper(nums,k-1);
+        return cnt;
+    }
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return solve(nums,k)-solve(nums,k-1);
     }
 };
