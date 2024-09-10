455ef1f231f658bbbc2b366688381b368d015fa7
Shasha0309 <sona2002sharma@gmail.com>
2024-09-10 10:34:23 +0530

    Time: 11 ms (76.59%), Space: 10.4 MB (71.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int hash[256] = {0};
         int l=0;
         int r=0;
-        int minLen = INT_MAX;
+        int minlen = 1e9;
+        int cnt = 0;
+        int strt = -1;
+        int hash[256] = {0};
         for(char c:t){
             hash[c]++;
         }
-        int cnt = 0;
-        int strt=-1;
         while(r<s.size()){
-            if(hash[s[r]]>0){
-                cnt++;
-            }
-                hash[s[r]]--;
+            if(hash[s[r]]>0) cnt++;
+            hash[s[r]]--;
             while(cnt==t.size()){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
-                    strt=l;
+                if(r-l+1<minlen){
+                    minlen = r-l+1;
+                    strt = l;
                 }
-                 hash[s[l]]++;
+                hash[s[l]]++;
                 if(hash[s[l]]>0){
                     cnt--;
                 }
@@ -28,6 +26,6 @@ public:
             }
             r++;
         }
-        return strt==-1 ? "":s.substr(strt,minLen);
+        return strt==-1 ? "" : s.substr(strt,minlen);
     }
 };
