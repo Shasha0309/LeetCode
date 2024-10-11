1700120ef74d880a3b95c71958e0f1f676a4d71d
Shasha0309 <sona2002sharma@gmail.com>
2024-10-11 19:03:55 +0530

    Time: 20 ms (6.01%), Space: 9.9 MB (31.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<int,int> mpp;
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+        for(char c:t){
+            if(mpp.find(c)==mpp.end() || mpp[c]==0) return false;
+            mpp[c]--;
+        }
+        return true;
+    }
+};
