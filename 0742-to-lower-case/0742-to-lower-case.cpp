9ce46365fcfcf400d6ff84b660b183bc42430589
Shasha0309 <sona2002sharma@gmail.com>
2024-10-12 15:14:49 +0530

    Time: 3 ms (41%), Space: 7.4 MB (99.94%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cpp
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.length();i++){
-            if(s[i]<97 && s[i]>='A' && s[i]<='Z') s[i]=(char)((s[i])+32);
+            if(s[i]>='A' && s[i]<='Z') s[i]=(char)((s[i])+32);
         }
         return s;
     }
