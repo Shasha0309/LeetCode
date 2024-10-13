99d606ace75fbd91f9c6b54cb593e8498dd23916
Shasha0309 <sona2002sharma@gmail.com>
2024-10-13 19:44:32 +0530

    Time: 79 ms (24.19%), Space: 86 MB (52.98%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -3,31 +3,25 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five = 0;
         int ten = 0;
-        int i = 0;
-        while(i<bills.size()){
-            if(bills[i]==5){
-            five++;
-           }
-           else if(bills[i]==10){
-            
-            if(five){
-                five--;
+        int twenty = 0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5) five++;
+            else if(bills[i]==10){
                 ten++;
-            }
-            else return false;
-           }
-           else{
-            
-            if(five && ten){
+                if(five==0) return false;
                 five--;
-                ten--;
             }
-            else if(five>2){
-                five-=3;
+            else{
+                twenty++;
+                if(five>0 && ten>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>2) five-=3;
+                else{
+                    return false;
+                }
             }
-            else return false;
-           }
-           i++;
         }
         return true;
     }
