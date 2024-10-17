db80c62fc1846dad3feca2d79635bd95077fbb60
Shasha0309 <sona2002sharma@gmail.com>
2024-10-17 09:45:20 +0530

    Time: 0 ms (100%), Space: 8.5 MB (60.34%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,34 +3,34 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int total = n*m;
+        int cnt = 0;
         int top = 0;
         int bottom = n-1;
         int left = 0;
         int right = m-1;
-        int total = n*m;
-        int cnt = 0;
         vector<int> ans;
         while(cnt<total){
-            for(int i=left;cnt<total && i<=right;i++){
-               ans.push_back(matrix[top][i]);
-               cnt++;
-            }
-            top++;
-            for(int i=top;cnt<total && i<=bottom;i++){
-                ans.push_back(matrix[i][right]);
-                cnt++;
-            }
-            right--;
-            for(int i=right;cnt<total && i>=left;i--){
-                ans.push_back(matrix[bottom][i]);
-                cnt++;
-            }
-            bottom--;
-            for(int i=bottom;cnt<total && i>=top;i--){
-                ans.push_back(matrix[i][left]);
-                cnt++;
-            }
-            left++;
+           for(int i=left;i<=right && cnt<total;i++){
+            ans.push_back(matrix[top][i]);
+            cnt++;
+           }
+           top++;
+           for(int i=top;i<=bottom && cnt<total;i++){
+            ans.push_back(matrix[i][right]);
+            cnt++;
+           }
+           right--;
+           for(int i=right;i>=left && cnt<total;i--){
+            ans.push_back(matrix[bottom][i]);
+            cnt++;
+           }
+           bottom--;
+           for(int i=bottom;i>=top && cnt<total;i--){
+            ans.push_back(matrix[i][left]);
+            cnt++;
+           }
+           left++;
         }
         return ans;
     }
