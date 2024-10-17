47ee7bf243a274a74051ec8ce076bab679d57763
Shasha0309 <sona2002sharma@gmail.com>
2024-10-17 09:58:31 +0530

    Time: 0 ms (100%), Space: 9 MB (56.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,12 +4,21 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                swap(matrix[i][j],matrix[j][i]);
+            for(int j=i+1;j<m;j++){
+                    swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+       int i=0;
+       int j=m-1;
+       int k=0;
+       while(k<n && i<=j){
+          swap(matrix[k][i],matrix[k][j]);
+          k++;
+          if(k==n){
+            k=0;
+            i++;
+            j--;
+          }
+       }
     }
 };
