b39f3f2c4cc5f642ea8e702692aa9b829736e76a
Shasha0309 <sona2002sharma@gmail.com>
2024-10-24 23:51:10 +0530

    Time: 7 ms (99.92%), Space: 58.2 MB (67.94%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,47 +9,22 @@
  * };
  */
 class Solution {
-    ListNode* solve(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-
-    ListNode* merge(ListNode* list1,ListNode* list2){
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
-                temp->next=list1;
-                temp=list1;
-                list1=list1->next;
-            }
-            else{
-                temp->next=list2;
-                temp=list2;
-                list2=list2->next;
-            }
-        }
-        if(list1) temp->next=list1;
-        else{
-            temp->next=list2;
-        }
-        return dummy->next;
-    }
 public:
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        ListNode* middle = solve(head);
-        ListNode* low = head;
-        ListNode* high = middle->next;
-        middle->next=NULL;
-
-        low = sortList(low);
-        high = sortList(high);
-        return merge(low,high);
+        ListNode* ptr = head;
+        vector<int> ans;
+        while(ptr){
+            ans.push_back(ptr->val);
+            ptr=ptr->next;
+        }
+        sort(ans.begin(),ans.end());
+        ptr=head;
+        int i=0;
+        while(ptr){
+            ptr->val=ans[i++];
+            ptr=ptr->next;
+        }
+        return head;
     }
 };
