7d0ef6fb01b98e864c3a63f8bdf7a84667ed5888
Shasha0309 <sona2002sharma@gmail.com>
2024-10-30 01:50:02 +0530

    Time: 24 ms (49.32%), Space: 22.2 MB (17.67%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,7 +15,7 @@ public:
         if(root==p || root==q) return root;
 
         if(root->val>p->val && root->val>q->val) return lowestCommonAncestor(root->left,p,q);
-        if(root->val<p->val && root->val<q->val) return lowestCommonAncestor(root->right,p,q);
-        else return root;
+         if(root->val<p->val && root->val<q->val) return lowestCommonAncestor(root->right,p,q);
+         else return root;
     }
 };
