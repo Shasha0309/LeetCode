2d7402dffd09a55c16530e1c1a847d56e51266b1
Shasha0309 <sona2002sharma@gmail.com>
2024-10-31 15:54:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -2,23 +2,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
         // Code here
-        int vis[V] = {0};
-        vis[0] = 1;
-        queue<int> q;
-        q.push(0);
+        int n=adj.size();
+        int vis[n] = {0};
         vector<int> bfs;
+        queue<int> q;
+        vis[0] = 1;
+        q.push(0);
         while(!q.empty()){
             int node = q.front();
             q.pop();
             bfs.push_back(node);
-            
-            for(auto it : adj[node]){
+            for(auto it:adj[node]){
                 if(!vis[it]){
                     vis[it] = 1;
                     q.push(it);
@@ -30,32 +31,33 @@ class Solution {
 };
 
 //{ Driver Code Starts.
+
 int main() {
     int tc;
     cin >> tc;
     while (tc--) {
         int V, E;
-        cin >> V >>
+        cin >> V >> E;
 
-            E;
-
-        vector<int> adj[V];
+        // Now using vector of vectors instead of array of vectors
+        vector<vector<int>> adj(V);
 
         for (int i = 0; i < E; i++) {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
-            // 		adj[v].push_back(u);
+            adj[v].push_back(u);
         }
-        // string s1;
-        // cin>>s1;
+
         Solution obj;
-        vector<int> ans = obj.bfsOfGraph(V, adj);
+        vector<int> ans = obj.bfsOfGraph(adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
         cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
