76d6ca6471d8595ed4242e13b38baf585a46bfa8
Shasha0309 <sona2002sharma@gmail.com>
2024-11-03 22:01:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -8,23 +8,22 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    int solve(int val[],int wt[],int W,int n,vector<vector<int>>& dp){
+    int solve(int n,int Wf,int val[],int wt[],vector<vector<int>>&dp){
         if(n==0){
-            return W/wt[0] * val[0];
+            return Wf/wt[0] * val[0]; 
         }
-        if(dp[n][W]!=-1) return dp[n][W];
-        long nottake = 0 + solve(val,wt,W,n-1,dp);
-        long take = INT_MIN;
-        if(wt[n]<=W) take = val[n] + solve(val,wt,W-wt[n],n,dp);
-        return dp[n][W] = max(nottake,take);
-       
+        if(dp[n][Wf]!=-1) return dp[n][Wf];
+        long notpick=0+solve(n-1,Wf,val,wt,dp);
+        long pick = INT_MIN;
+        if(wt[n]<=Wf) pick = val[n]+solve(n,Wf-wt[n],val,wt,dp);
+        return dp[n][Wf]=max(notpick,pick);
     }
 public:
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-        vector<vector<int>> dp(N, vector<int>(W+1,-1));
-        return solve(val,wt,W,N-1,dp);
+        vector<vector<int>> dp(N,vector<int>(W+1,-1));
+        return solve(N-1,W,val,wt,dp);
     }
 };
 
@@ -44,7 +43,9 @@ int main(){
         
         Solution ob;
         cout<<ob.knapSack(N, W, val, wt)<<endl;
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
