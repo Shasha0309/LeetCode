967d6e6c9fdc83ae4eeaccfa3b041f4e883869ea
Shasha0309 <sona2002sharma@gmail.com>
2024-11-03 19:41:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    int mod = (int)(1e9+7);
-    long long int solve(int arr[],int n, int sum,vector<vector<int>>& dp){
-        if(n==0){
-        if(sum==0 && arr[0]==0) return 2;
-        if(sum==0 || arr[0]==sum) return 1;
-        return 0;
-        }
-        
-        if(dp[n][sum] != -1) return dp[n][sum];
-        int nottake = solve(arr,n-1,sum,dp);
-        int take = 0;
-        if(arr[n]<=sum) take = solve(arr,n-1,sum-arr[n],dp);
-        
-        return dp[n][sum] = (nottake + take) % mod;
-    }
+
 	public:
+	int mod = (int)(1e9+7);
+	long long int solve(int arr[],int sum,int ind,vector<vector<int>>& dp){
+	    if(ind==0){
+	        if(sum==0 && arr[0]==sum) return 2;
+	        else if(sum==0 || arr[0]==sum) return 1;
+	        else return 0;
+	    }
+	    if(dp[ind][sum]!=-1) return dp[ind][sum];
+	    int notpick = solve(arr,sum,ind-1,dp);
+	    int pick = 0;
+	    if(arr[ind]<=sum) pick = solve(arr,sum-arr[ind],ind-1,dp);
+	    return dp[ind][sum] = (notpick+pick)%mod;
+	}
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-        vector<vector<int>> dp(n, vector<int>(sum+1,-1));
-        return solve(arr,n-1,sum,dp);
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        return solve(arr,sum,n-1,dp);
 	}
 	  
 };
@@ -51,7 +50,9 @@ int main()
 	    Solution ob;
 	    cout << ob.perfectSum(a, n, sum) << "\n";
 	     
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 
