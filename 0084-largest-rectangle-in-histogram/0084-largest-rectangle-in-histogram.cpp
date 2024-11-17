04fb58beff0ba8ebb5f3c6cbb5570fe9f8e20c0f
Shasha0309 <sona2002sharma@gmail.com>
2024-11-17 23:01:49 +0530

    Time: 37 ms (50.11%), Space: 79 MB (82.34%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,70 @@
+class Solution {
+    void findnse(int nse[],const vector<int>& arr){
+        int n = arr.size();
+        stack<int> st;
+        for(int i=arr.size()-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            nse[i] = st.empty() ? n-1:st.top()-1;
+            st.push(i);
+        }
+    }
+    
+    void findpse(int pse[],const vector<int>& arr){
+        stack<int> st;
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            pse[i] = st.empty() ? 0:st.top()+1;
+            st.push(i);
+        }
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+       int n = heights.size();
+        int nse[n];
+        int pse[n];
+        findnse(nse,heights);
+        findpse(pse,heights);
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            cnt = max(cnt,(nse[i]-pse[i]+1)*heights[i]);
+        }
+        return cnt;
+        /*int n = heights.size();
+      stack < int > st;
+      int leftsmall[n], rightsmall[n];
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && heights[st.top()] >= heights[i]) {
+          st.pop();
+        }
+        if (st.empty())
+          leftsmall[i] = 0;
+        else
+          leftsmall[i] = st.top() + 1;
+        st.push(i);
+      }
+      // clear the stack to be re-used
+      while (!st.empty())
+        st.pop();
+
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && heights[st.top()] >= heights[i])
+          st.pop();
+
+        if (st.empty())
+          rightsmall[i] = n - 1;
+        else
+          rightsmall[i] = st.top() - 1;
+
+        st.push(i);
+      }
+      int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;*/
+    }
+};
