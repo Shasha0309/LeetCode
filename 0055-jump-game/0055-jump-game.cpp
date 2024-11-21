933dc5043e2a2782b04ed4adc1217006d19e9900
Shasha0309 <sona2002sharma@gmail.com>
2024-11-21 17:15:46 +0530

    Time: 0 ms (100%), Space: 51.2 MB (21.48%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int farthest = 0;
+        int maxi = 0;
         for(int i=0;i<nums.size();i++){
-            if(i>farthest) return false;
-            farthest = max(farthest,i+nums[i]);
+            if(i>maxi) return false;
+            maxi = max(maxi,nums[i]+i);
         }
         return true;
     }
