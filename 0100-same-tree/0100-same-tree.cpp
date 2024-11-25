dac0ed54d5c4cffca739177cb9e3898187d881b0
Shasha0309 <sona2002sharma@gmail.com>
2024-11-26 01:20:12 +0530

    Time: 0 ms (100%), Space: 12.2 MB (13.36%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,6 +14,5 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL) return p==q;
         return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
-        
     }
 };
