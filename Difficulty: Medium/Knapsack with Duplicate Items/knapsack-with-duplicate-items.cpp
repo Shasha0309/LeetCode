5a9f462693a87f5e83f6df7cba9a7d60e490e380
Shasha0309 <sona2002sharma@gmail.com>
2024-12-10 18:04:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -4,48 +4,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-    int solve(int n,int Wf,int val[],int wt[],vector<vector<int>>&dp){
-        if(n==0){
-            return Wf/wt[0] * val[0]; 
+class Solution {
+    int solve(vector<int>& val,vector<int>& wt,vector<vector<int>>& dp,int ind,int tar){
+        if(ind==0){
+        
+                return tar/wt[0]*val[0];
+            
+            
         }
-        if(dp[n][Wf]!=-1) return dp[n][Wf];
-        long notpick=0+solve(n-1,Wf,val,wt,dp);
-        long pick = INT_MIN;
-        if(wt[n]<=Wf) pick = val[n]+solve(n,Wf-wt[n],val,wt,dp);
-        return dp[n][Wf]=max(notpick,pick);
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+        long ntake = solve(val,wt,dp,ind-1,tar);
+        long take = -1e9;
+        if(wt[ind]<=tar) take = val[ind]+solve(val,wt,dp,ind,tar-wt[ind]);
+        return dp[ind][tar] = max(take,ntake);
     }
-public:
-    int knapSack(int N, int W, int val[], int wt[])
-    {
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        vector<vector<int>> dp(N,vector<int>(W+1,-1));
-        return solve(N-1,W,val,wt,dp);
+        int n = val.size();
+        vector<vector<int>> dp(n,vector<int>(capacity+1,-1));
+        return solve(val,wt,dp,n-1,capacity);
     }
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int N, W;
-        cin>>N>>W;
-        int val[N], wt[N];
-        for(int i = 0;i < N;i++)
-            cin>>val[i];
-        for(int i = 0;i < N;i++)
-            cin>>wt[i];
-        
+    cin >> t;
+    while (t--) {
+        int W;
+        cin >> W;
+        cin.ignore();
+        string str;
+        getline(cin, str);
+        stringstream ss(str);
+        vector<int> val;
+        int num;
+        while (ss >> num) {
+            val.push_back(num);
+        }
+        string str2;
+        getline(cin, str2);
+        stringstream ss2(str2);
+        vector<int> wt;
+        int num2;
+        while (ss2 >> num2) {
+            wt.push_back(num2);
+        }
         Solution ob;
-        cout<<ob.knapSack(N, W, val, wt)<<endl;
-    
-cout << "~" << "\n";
-}
+        cout << ob.knapSack(val, wt, W) << endl;
+        cout << "~" << endl;
+    }
     return 0;
 }
 // } Driver Code Ends
