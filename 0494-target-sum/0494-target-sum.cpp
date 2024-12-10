0313a371a9180b7c30d70b8f12622377c8203566
Shasha0309 <sona2002sharma@gmail.com>
2024-12-10 17:27:50 +0530

    Time: 7 ms (71.19%), Space: 15.2 MB (41.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,32 +1,26 @@
 class Solution {
-    int sol(vector<int>& arr,int target,int n){
-        vector<vector<int>> dp(n, vector<int>(target+1,0));
-        if(arr[0]==0) dp[0][0] = 2;
-        else dp[0][0]=1;
-        if(arr[0]!=0 && arr[0]<=target) dp[0][arr[0]]=1;
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=target;j++){
-                int nottake = dp[i-1][j];
-                int take = 0;
-                if(arr[i]<=j) take = dp[i-1][j-arr[i]];
-                dp[i][j] = (nottake + take);
-            }
+    int solve(vector<int>& nums,vector<vector<int>>& dp,int ind,int tar){
+        if(ind==0){
+            if(tar==0 && nums[0]==tar) return 2;
+            else if(tar==0 || nums[0]==tar) return 1;
+            else return 0;
         }
-        return dp[n-1][target];
-    }
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int totsum = 0;
-        for(int i=0;i<n;i++){
-            totsum+=arr[i];
-        }
-        if(totsum-d<0 || (totsum-d)%2!=0) return false;
-        return sol(arr,(totsum-d)/2,n);
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+        int ntake = solve(nums,dp,ind-1,tar);
+        int take = 0;
+        if(nums[ind]<=tar) take = solve(nums,dp,ind-1,tar-nums[ind]);
+        return dp[ind][tar] = ntake+take;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n= nums.size();
-        return countPartitions(n,target,nums);
+        int n = nums.size();
+        int totsum = 0;
+        for(int i=0;i<n;i++){
+            totsum+=nums[i];
+        }
+        if(totsum<target || (totsum-target)%2!=0) return 0;
+        int k = (totsum-target)/2;
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        return solve(nums,dp,n-1,k);
     }
 };
