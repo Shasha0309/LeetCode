69cfda9bf0272489285abde479c9310f959e825a
Shasha0309 <sona2002sharma@gmail.com>
2024-12-10 17:49:29 +0530

    Time: 14 ms (79.22%), Space: 28.4 MB (28.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
+    int mod = (int)1e9+7;
+    int solve(vector<int>& arr,vector<vector<int>>& dp,int ind,int tar){
+        if(ind==0) return tar%arr[0]==0;
+    
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+
+        int ntake = solve(arr,dp,ind-1,tar);
+        int take = 0;
+        if(arr[ind]<=tar) take = solve(arr,dp,ind,tar-arr[ind]);
+        return dp[ind][tar] = ntake+take;
+    }
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-            dp[0][i] = 1;
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                long nottake = dp[i-1][j];
-                long take = 0;
-                if(coins[i]<=j) take = dp[i][j-coins[i]];
-                dp[i][j] = nottake + take;
-            }
-        }
-        return dp[n-1][amount];
+        int n= coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans = solve(coins,dp,n-1,amount);
+      //  if(ans==1e9) return 0;
+        return ans;
     }
 };
