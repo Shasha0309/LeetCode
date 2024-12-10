e3aa317ddb701f4e14c55a4e3f06a9a02bb0c3e2
Shasha0309 <sona2002sharma@gmail.com>
2024-12-10 15:10:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,51 +1,23 @@
 class Solution {
-    bool subsetSumUtil(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
-    // Base case: If the target sum is 0, we found a valid partition
-    if (target == 0)
-        return true;
-
-    // Base case: If we have considered all elements and the target is still not 0, return false
-    if (ind == 0)
-        return arr[0] == target;
-
-    // If the result for this state is already calculated, return it
-    if (dp[ind][target] != -1)
-        return dp[ind][target];
-
-    // Recursive cases
-    // 1. Exclude the current element
-    bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
-
-    // 2. Include the current element if it doesn't exceed the target
-    bool taken = false;
-    if (arr[ind] <= target)
-        taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
-
-    // Store the result in the DP table and return
-    return dp[ind][target] = notTaken || taken;
-}
-
+    bool solve(vector<int>& nums,vector<vector<int>>& dp,int tar,int ind){
+        if(tar==0) return true;
+        if(ind==0) return nums[0]==tar;
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+        bool ntake = solve(nums,dp,tar,ind-1);
+        bool take = false;
+        if(nums[ind]>=tar) take = solve(nums,dp,tar-nums[ind],ind-1);
+        return dp[ind][tar] = (take || ntake);
+    }
 public:
     bool canPartition(vector<int>& nums) {
-        int totSum = 0;
-
-    // Calculate the total sum of the array
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        totSum += nums[i];
-    }
-
-    // If the total sum is odd, it cannot be partitioned into two equal subsets
-    if (totSum % 2 == 1)
-        return false;
-    else {
-        int k = totSum / 2;
-
-        // Create a DP table with dimensions n x k+1 and initialize with -1
-        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-
-        // Call the subsetSumUtil function to check if it's possible to partition
-        return subsetSumUtil(n - 1, k, nums, dp);
-    }
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0) return false;
+        int k = sum/2;
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        return solve(nums,dp,k,n-1);
     }
 };
