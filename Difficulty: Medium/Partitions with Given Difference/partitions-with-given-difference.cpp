c235b5d0a4d901d4dff520f04df3ddf645549f6c
Shasha0309 <sona2002sharma@gmail.com>
2024-12-10 16:03:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,55 +1,71 @@
 //{ Driver Code Starts
+// Initial function template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
-     int mod = (int)(1e9+7);
-    int sol(vector<int>& arr,int target,int n){
-        vector<vector<int>> dp(n, vector<int>(target+1,0));
-        if(arr[0]==0) dp[0][0] = 2;
-        else dp[0][0]=1;
-        if(arr[0]!=0 && arr[0]<=target) dp[0][arr[0]]=1;
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=target;j++){
-                int nottake = dp[i-1][j];
-                int take = 0;
-                if(arr[i]<=j) take = dp[i-1][j-arr[i]];
-                dp[i][j] = (nottake + take) % mod;
-            }
+    int mod = (int)1e9+7;
+    int solve(vector<int>& arr,vector<vector<int>>& dp,int ind,int tar){
+        if(ind==0){
+            if(tar==0 && tar==arr[0]) return 2;
+            else if(tar==0 || tar==arr[0]) return 1;
+            else return 0;
         }
-        return dp[n-1][target];
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+        int ntake = solve(arr,dp,ind-1,tar);
+        int take = 0;
+        if(arr[ind]<=tar) take = solve(arr,dp,ind-1,tar-arr[ind]);
+        return dp[ind][tar] = (take+ntake)% mod;
     }
-public:
-    int countPartitions(int n, int d, vector<int>& arr) {
+  public:
+    int countPartitions(vector<int>& arr, int d) {
         // Code here
-        int totsum = 0;
+        int n = arr.size();
+        int sum = 0;
         for(int i=0;i<n;i++){
-            totsum+=arr[i];
+            sum+=arr[i];
         }
-        if(totsum-d<0 || (totsum-d)%2!=0) return false;
-        return sol(arr,(totsum-d)/2,n);
+        if(sum-d<0 || (sum-d)%2!=0) return false;
+        int k = (sum-d)/2;
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        return solve(arr,dp,n-1,k);
     }
 };
 
 //{ Driver Code Starts.
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, d;
-        cin >> n >> d;
-        vector<int> arr;
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
 
-        for (int i = 0; i < n; ++i) {
-            int x;
-            cin >> x;
-            arr.push_back(x);
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countPartitions(arr, d);
+        cout << ans << endl;
 
-        Solution obj;
-        cout << obj.countPartitions(n, d, arr) << "\n";
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
