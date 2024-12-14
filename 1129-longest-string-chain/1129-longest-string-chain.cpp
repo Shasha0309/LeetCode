37979085cbc6e867d87aefc1746c7cd65ba88403
Shasha0309 <sona2002sharma@gmail.com>
2024-12-14 15:29:03 +0530

    Time: 1838 ms (20.55%), Space: 640.3 MB (12.17%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,39 +1,39 @@
 class Solution {
-    bool ifposib(string &s1,string &s2){
-        if(s1.size()!=s2.size()+1) return false;
-        int first = 0;
-        int second = 0;
-        while(first<s1.size()){
-            if(s1[first]==s2[second]){
+    bool compare(string word1,string word2){
+        if(word1.size()!=word2.size()+1) return false;
+        int first=0;
+        int sec=0;
+        //int n = word1;
+        //int m = word2;
+        while(first<word1.size()){
+            if(word1[first]==word2[sec] && sec<word2.size()){
                 first++;
-                second++;
+                sec++;
             }
             else{
                 first++;
             }
         }
-        if(first==s1.size() && second==s2.size()) return true;
-        return false;
+        return first==word1.size() && sec==word2.size();
     }
+
     bool static comp(string& s1,string& s2){
         return s1.size()<s2.size();
     }
 public:
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),comp);
         int n = words.size();
-        vector<int> dp(n,1);
-        int maxi = 1;
-        for(int i=0;i<words.size();i++){
-            for(int j=0;j<i;j++){
-                if(ifposib(words[i],words[j]) && 1+dp[j]>dp[i]){
-                    dp[i] = 1+dp[j];
+        sort(words.begin(),words.end(),comp);
+        int ans=0;
+        vector<int> dp(n,1); //hash(n,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<=i-1;prev++){
+                if(compare(words[i],words[prev])){
+                    dp[i]=max(dp[i],1+dp[prev]);
                 }
             }
-            if(maxi<dp[i]){
-                maxi = dp[i];
-            }
+            ans = max(ans,dp[i]);
         }
-        return maxi;
+        return ans;
     }
 };
