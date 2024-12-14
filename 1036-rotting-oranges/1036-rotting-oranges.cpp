74b3f1c7f9c320db6e799b4af543b4c8ed3a2f73
Shasha0309 <sona2002sharma@gmail.com>
2024-12-15 01:23:04 +0530

    Time: 0 ms (100%), Space: 16.8 MB (29.05%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        if(grid.empty()) return 0;
         int n = grid.size();
         int m = grid[0].size();
-        int cnt = 0;
         queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,0));
+          vector<vector<int>> vis(n,vector<int>(m,0));
+        int total=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
+                    vis[i][j]=2;
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
                 }
-                else{
-                    vis[i][j] = 0;
+                if(grid[i][j]==1){
+                    total++;
                 }
-                if(grid[i][j]==1) cnt++;
             }
         }
+        int cnt = 0;
         int tm = 0;
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,1,0,-1};
-        int cfrt = 0;
+      
+        int ro[] = {1,0,-1,0};
+        int co[] = {0,1,0,-1};
         while(!q.empty()){
-           int r = q.front().first.first;
-           int c = q.front().first.second;
-           int tis = q.front().second;
-           q.pop();
-           tm=max(tm,tis);
-           for(int i=0;i<4;i++){
-            int rrow=drow[i]+r;
-            int ccol=dcol[i]+c;
-            if(rrow>=0 && rrow<n && ccol>=0 && ccol<m && grid[rrow][ccol]==1 && vis[rrow][ccol]!=2){
-                q.push({{rrow,ccol},tis+1});
-                vis[rrow][ccol]=2;
-                cfrt++;
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int tis = q.front().second;
+            q.pop();
+            tm = max(tm,tis);
+            for(int i=0;i<4;i++){
+                int drow = ro[i]+x;
+                int dcol = co[i]+y;
+                if(drow>=0 && drow<n && dcol>=0 && dcol<m && grid[drow][dcol]==1 && vis[drow][dcol]!=2){
+                    vis[drow][dcol] = 2;
+                    q.push({{drow,dcol},tis+1});
+                    cnt++;
+                }
             }
-           }
         }
-        if(cfrt!=cnt) return -1;
+        if(cnt!=total) return -1;
         return tm;
     }
 };
