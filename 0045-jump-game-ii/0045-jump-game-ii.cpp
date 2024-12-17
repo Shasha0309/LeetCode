e91f5a4283c325bce283beed56a88f41d4dea836
Shasha0309 <sona2002sharma@gmail.com>
2024-12-18 02:51:34 +0530

    Time: 0 ms (100%), Space: 20.4 MB (36.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,11 +5,12 @@ public:
         int farth=0;
         int l=0;
         int r=0;
-        while(r<nums.size()){
-            if(r==nums.size()-1) return jump;
-           farth=max(nums[l],nums[r]);
+        while(r<nums.size()-1){
+            for(int i=l;i<=r;i++){
+           farth=max(farth,nums[i]+i);
+            }
            l=r+1;
-           r+=farth; 
+           r=farth; 
            jump++;
         }
         return jump;
