4cab9dc02d7870c92832b876c7a2bf7050d60fe6
Shasha0309 <sona2002sharma@gmail.com>
2024-12-19 01:41:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,16 +4,17 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        int n = intervals.size();
-        for(int i=1;i<n;i++){
+        int i=1;
+        while(i<intervals.size()){
             if(ans.back()[1]>=intervals[i][0]){
-                if(ans.back()[1]>intervals[i][1]) continue;
+                if(ans.back()[1]>=intervals[i][1]) continue;
                 ans.back()[1]=intervals[i][1];
             }
             else{
                 ans.push_back(intervals[i]);
             }
-    }
-    return ans;
+            i++;
+        }
+        return ans;
     }
 };
