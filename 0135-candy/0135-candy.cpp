8ca843c6aae89fab5fa41e93902ff568aeeb45d7
Shasha0309 <sona2002sharma@gmail.com>
2024-12-18 21:24:23 +0530

    Time: 0 ms (100%), Space: 20.8 MB (46.24%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> larr(ratings.size(),1);
-        vector<int> rarr(ratings.size(),1);
-        int tot = 0;
-        
-        for(int i=1;i<ratings.size();i++){
-            if(ratings[i]>ratings[i-1]){
-                larr[i]=larr[i-1]+1;
+        int n = ratings.size();
+        int sum = 1;
+        int i=1;
+        while(i<n){
+            while(i<n && ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+            }
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down+=1;
+                i++;
+            }
+            if(down>peak){
+                sum += (down-peak);
             }
         }
-        for(int i=ratings.size()-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                rarr[i]=rarr[i+1]+1;
-            }
-        }
-        for(int i=0;i<ratings.size();i++){
-            tot+=max(larr[i],rarr[i]);
-        }
-        return tot;
+        return sum;
     }
 };
