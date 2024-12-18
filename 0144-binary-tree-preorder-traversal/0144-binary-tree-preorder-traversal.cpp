1e6ea19a9aeb3ef943e3ca536dd25c6b77d9a0f6
Shasha0309 <sona2002sharma@gmail.com>
2024-12-19 03:37:53 +0530

    Time: 0 ms (100%), Space: 10.9 MB (33.26%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -10,22 +10,16 @@
  * };
  */
 class Solution {
+    void solve(vector<int>& ans,TreeNode* root){
+        if(root==NULL) return;
+        ans.push_back(root->val);
+        solve(ans,root->left);
+        solve(ans,root->right);
+    }
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(root==NULL) return ans;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            int n = st.size();
-            for(int i=0;i<n;i++){
-                TreeNode* nef = st.top();
-                st.pop();
-                ans.push_back(nef->val);
-                if(nef->right!=NULL) st.push(nef->right);
-                if(nef->left!=NULL) st.push(nef->left);
-            }
-        }
+        solve(ans,root);
         return ans;
     }
 };
