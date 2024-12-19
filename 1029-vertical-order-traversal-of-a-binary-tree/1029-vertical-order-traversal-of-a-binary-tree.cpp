57962fb904b2747307e48fa61638c9f8ca1163d5
Shasha0309 <sona2002sharma@gmail.com>
2024-12-20 04:09:12 +0530

    Time: 3 ms (44.2%), Space: 16.6 MB (5.14%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -10,29 +10,35 @@
  * };
  */
 class Solution {
+    
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         map<int,map<int,multiset<int>>> mpp;
         queue<pair<TreeNode*,pair<int,int>>> q;
-        vector<vector<int>> ans;
         q.push({root,{0,0}});
         while(!q.empty()){
-            auto p = q.front();
+            TreeNode* node = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
             q.pop();
-            int x = p.second.first;
-            int y = p.second.second;
-            TreeNode* node = p.first; 
             mpp[x][y].insert(node->val);
-            if(node->left) q.push({node->left,{x-1,y+1}});
-            if(node->right) q.push({node->right,{x+1,y+1}});
-            }
-            for(auto p:mpp){
-                vector<int> col;
-                for(auto q:p.second){
-                    col.insert(col.end(),q.second.begin(),q.second.end());
+                if(node->left){
+                    q.push({node->left,{x-1,y+1}});
                 }
-                ans.push_back(col);
+                if(node->right!=NULL){
+                    q.push({node->right,{x+1,y+1}});
+                }
+                
+            
+        }
+        vector<vector<int>> ans;
+        for(auto it:mpp){
+            vector<int> col;
+            for(auto p:it.second){
+                col.insert(col.end(),p.second.begin(),p.second.end());
             }
-            return ans;
+            ans.push_back(col);
+        }
+        return ans;
     }
 };
