78e086d76f72fc74ee90bbd2a180a0e1d2b83a10
Shasha0309 <sona2002sharma@gmail.com>
2025-01-06 18:30:28 +0530

    Time: 0 ms (100%), Space: 22 MB (11.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-       int n = nums.size();
-       int total = 0;
-        for(int i=0;i<n;i++){
-            total = total+nums[i];
+        int total = 0;
+        int n = nums.size();
+        for(int i=0;i<nums.size();i++){
+            total += nums[i];
         }
-          long long totalSum = static_cast<long long>(n) * (n + 1) / 2;
-          return totalSum-total;
+        long long res = static_cast<long long> (n)*(n+1)/2;
+        return res-total;
     }
 };
