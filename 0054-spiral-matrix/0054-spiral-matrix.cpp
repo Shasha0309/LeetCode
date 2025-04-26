93928acc9891d695d5cf281eb95cd6327a6c3c10
Shasha0309 <sona2002sharma@gmail.com>
2025-04-26 16:08:54 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.89%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
         int n = matrix.size();
         int m = matrix[0].size();
         int total = n*m;
         int cnt = 0;
-        int top = 0;
-        int bottom = n-1;
-        int left = 0;
-        int right = m-1;
-        vector<int> ans;
+        int srow = 0;
+        int erow = n-1;
+        int scol = 0;
+        int ecol = m-1; 
         while(cnt<total){
-           for(int i=left;i<=right && cnt<total;i++){
-            ans.push_back(matrix[top][i]);
-            cnt++;
-           }
-           top++;
-           for(int i=top;i<=bottom && cnt<total;i++){
-            ans.push_back(matrix[i][right]);
-            cnt++;
-           }
-           right--;
-           for(int i=right;i>=left && cnt<total;i--){
-            ans.push_back(matrix[bottom][i]);
-            cnt++;
-           }
-           bottom--;
-           for(int i=bottom;i>=top && cnt<total;i--){
-            ans.push_back(matrix[i][left]);
-            cnt++;
-           }
-           left++;
+            for(int i=scol;i<=ecol && cnt<total;i++){
+                ans.push_back(matrix[srow][i]);
+                cnt++;
+            }
+            srow++;
+            for(int i=srow;i<=erow && cnt<total;i++){
+                ans.push_back(matrix[i][ecol]);
+                cnt++;
+            }
+            ecol--;
+            for(int i=ecol;i>=scol && cnt<total;i--){
+                ans.push_back(matrix[erow][i]);
+                cnt++;
+            }
+            erow--;
+            for(int i=erow;i>=srow && cnt<total;i--){
+                ans.push_back(matrix[i][scol]);
+                cnt++;
+            }
+            scol++;
         }
         return ans;
     }
