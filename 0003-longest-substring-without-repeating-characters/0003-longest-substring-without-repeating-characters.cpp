909b1732269a1d12f26cfd8182a2c447dce7c3b1
Shasha0309 <sona2002sharma@gmail.com>
2025-05-24 16:08:47 +0530

    Time: 15 ms (41.14%), Space: 14.4 MB (15.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       /* if(s.length()==0) return 0;
-        int maxlen = INT_MIN;
-        for(int i=0;i<s.length();i++){
-            unordered_set<char> st;
-            for(int j=i;j<s.length();j++){
-                if(st.find(s[j])!=st.end()){
-                    maxlen = max(maxlen,j-i);
-                    break;
-                }
-                st.insert(s[j]);
+        int left = 0;
+        int ans = 0;
+        unordered_set<char> st;
+        for(int rig = 0;rig<s.length();rig++){
+            while(st.find(s[rig])!=st.end()){
+                st.erase(s[left]);
+                left++;
             }
+            st.insert(s[rig]);
+            ans = max(ans,rig-left+1);
         }
-        return maxlen;*/
-          unordered_set<char> st; // To store unique characters in the current window
-    int maxlen = 0;         // To track the maximum length of substring
-    int left = 0;           // Left pointer for the sliding window
-
-    for (int right = 0; right < s.length(); right++) {
-        // If a duplicate character is found, shrink the window from the left
-        while (st.find(s[right]) != st.end()) {
-            st.erase(s[left]);
-            left++;
-        }
-        // Add the current character to the set
-        st.insert(s[right]);
-        // Update the maximum length
-        maxlen = max(maxlen, right - left + 1);
-    }
-
-    return maxlen;
+        return ans;
     }
 };
