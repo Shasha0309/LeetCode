5e2ca9d30e5a74e356892f85eb3f4dd80c2b47a9
Shasha0309 <sona2002sharma@gmail.com>
2025-06-06 20:14:43 +0530

    Time: 3 ms (15.92%), Space: 9.2 MB (24.97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int myAtoi(string s) {
-         int len = s.size();
         double num = 0;
         int i=0;
-        while(s[i] == ' '){
+        while(s[i]==' '){
             i++;
         }
-        bool positive = s[i] == '+';
-        bool negative = s[i] == '-';
-        positive == true ? i++ : i;
-        negative == true ? i++ : i;
-        while(i < len && s[i] >= '0' && s[i] <= '9'){
-            num = num*10 + (s[i]-'0');
+        bool pos = s[i] == '+' ;
+        bool neg = s[i] == '-' ;
+        pos == true ? i++ : i;
+        neg == true ? i++ : i;
+        while(i<s.size() && s[i]>='0' && s[i]<='9'){
+            num = (num*10) + (s[i]-'0');
             i++;
         }
-        num = negative ? -num : num;
-        //cout<<num<<endl;
-        num = (num > INT_MAX) ? INT_MAX : num;
-        num = (num < INT_MIN) ? INT_MIN : num;
-        //cout<<num<<endl;
+        num = neg ? -num : num;
+        num = (num >INT_MAX) ? INT_MAX : num;
+        num = (num<INT_MIN) ? INT_MIN : num;
         return int(num);
     }
 };
