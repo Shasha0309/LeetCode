ae9c055c0948c540b42aa3c345e8fc9f8a505565
Shasha0309 <sona2002sharma@gmail.com>
2025-06-14 00:23:17 +0530

    Time: 23 ms (57.21%), Space: 17.5 MB (59.07%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,30 +3,24 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int p=i+1;p<n;p++){
-                if (p > i + 1 && nums[p]==nums[p - 1]) continue;
-            int j = p+1;
-            int k = n-1;
-            while(j<k){
-               long long sum = nums[i];
-               sum += nums[j];
-                sum += nums[k];
-                sum += nums[p];
-                if(sum>target) k--;
-                else if(sum<target) j++;
-                else{
-                    vector<int> temp = {nums[i],nums[j],nums[k],nums[p]};
-                    sort(temp.begin(),temp.end());
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
+            for(int j=i+1;j<nums.size();j++){
+              if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int left = j+1;
+                int right = nums.size()-1;
+                while(left<right){
+                    long long sum = (long long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        while(left<right && nums[left]==nums[left+1]) left++;
+                        while(left<right && nums[right]==nums[right-1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if(sum<target) left++;
+                    else right--;
                 }
-            }
             }
         }
         return ans;
