b0724ee05caf91142e737e952d22dcb0e3df1e6f
Shasha0309 <sona2002sharma@gmail.com>
2025-06-16 18:24:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.97%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -5,20 +5,20 @@ public:
         bool sign = true;
         if(dividend>=0 && divisor<0) sign=false;
         if(dividend<=0 && divisor>0) sign=false;
+
         long n = abs((long)dividend);
         long d = abs((long)divisor);
-        //divisor=abs(divisor);
         int ans=0;
         while(n>=d){
-            int cnt=0;
-            while(n >= (d<<(cnt+1))){
-                cnt++;
+            int cnt = 0;
+            while(n>=(d<<(cnt+1))){
+               cnt++;
             }
             ans += (1<<cnt);
             n -= (d<<cnt);
         }
         if(ans==(1<<31) && sign) return INT_MAX;
         if(ans==(1<<31) && !sign) return INT_MIN;
-        return sign ? ans:-ans;
+        return sign? ans:-ans;
     }
 };
