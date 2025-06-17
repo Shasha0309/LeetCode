7af0a31c77cff1e782ffbb1c6e9f2a5aaeb6883e
Shasha0309 <sona2002sharma@gmail.com>
2025-06-17 21:15:19 +0530

    Time: 0 ms (100%), Space: 15.5 MB (99.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,16 +4,16 @@ public:
         int les = -1;
         for(int i=nums.size()-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                les = i;
+                les=i;
                 break;
             }
         }
         for(int k=nums.size()-1;k>les && les!=-1;k--){
-                    if(nums[k]>nums[les]){
-                        swap(nums[les],nums[k]);
-                        break;
-                    }
-                }
+            if(nums[k]>nums[les]){
+                swap(nums[k],nums[les]);
+                break;
+            }
+        }
         reverse(nums.begin()+les+1,nums.end());
     }
 };
