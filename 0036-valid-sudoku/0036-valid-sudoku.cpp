7ba507fa0bd9e1160ad6be27317d57676abd97d9
Shasha0309 <sona2002sharma@gmail.com>
2025-06-29 21:19:43 +0530

    Time: 3 ms (63.23%), Space: 25.6 MB (25.59%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<int> rows[9];
-        unordered_set<int> cols[9];
-        unordered_set<int> boxes[9];
+        unordered_set<int> row[9];
+        unordered_set<int> col[9];
+        unordered_set<int> box[9];
 
         for(int r=0;r<9;r++){
             for(int c=0;c<9;c++){
-                if(board[r][c]=='.'){
-                    continue;
-                }
-                char value=board[r][c];
-                int box_index=(r/3)*3+(c/3);
-                if(rows[r].count(value) || cols[c].count(value) || boxes[box_index].count(value)){
-                    return false;
-                }
+                if(board[r][c]=='.') continue;
+                char val = board[r][c];
+                int box_ind = (r/3)*3 + (c/3);
+                if(row[r].count(val) || col[c].count(val) || box[box_ind].count(val)) return false;
 
-                rows[r].insert(value);
-                cols[c].insert(value);
-                boxes[box_index].insert(value);
+               row[r].insert(val);
+               col[c].insert(val);
+               box[box_ind].insert(val); 
             }
         }
         return true;
