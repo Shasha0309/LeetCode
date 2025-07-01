54916585eebd290da1ac0df85852c7745fbd2216
Shasha0309 <sona2002sharma@gmail.com>
2025-07-01 17:22:13 +0530

    Time: 1 ms (26.6%), Space: 27.4 MB (8.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,26 +1,23 @@
 class Solution {
-
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int total = 0;
-        int leftmax[n];
-        int rightmax[n];
-        leftmax[0] = height[0];
-        rightmax[n-1] = height[n-1];
+        vector<int> maxl(n,0);
+        vector<int> maxr(n,0);
+        maxl[0] = height[0];
+        maxr[height.size()-1] = height[height.size()-1];
         for(int i=1;i<n;i++){
-            leftmax[i] = max(leftmax[i-1],height[i]);
+            if(height[i]>maxl[i-1]) maxl[i]=height[i];
+            else maxl[i]=maxl[i-1];
         }
-        for(int i=n-2;i>=0;i--){
-            rightmax[i] = max(rightmax[i+1],height[i]);
+        for(int i=height.size()-2;i>=0;i--){
+            if(height[i]>maxr[i+1]) maxr[i]=height[i];
+            else maxr[i]=maxr[i+1];
         }
-        for(int i=1;i<n-1;i++){
-            int lefta = leftmax[i];
-            int riga = rightmax[i];
-            if(height[i]<lefta && height[i]<riga){
-                total += (min(lefta,riga)-height[i]);
-            }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = ans+abs(height[i]-(min(maxl[i],maxr[i])));
         }
-        return total;
+        return ans;
     }
 };
