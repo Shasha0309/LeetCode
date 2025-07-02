d882a286881bddfb07299929bd108319ca7c794e
Shasha0309 <sona2002sharma@gmail.com>
2025-07-02 20:33:53 +0530

    Time: 39 ms (23.12%), Space: 14.7 MB (74.41%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,25 +1,45 @@
 class Solution {
     bool sol(int i,int j,string& s,string& p,vector<vector<int>>& dp){
-        if(i<0 && j<0) return 1;
-        if(i>=0 && j<0) return 0;
-        if(i<0 && j>=0){
-            for(int ii=0;ii<=j;ii++){
-                if(p[ii]!='*') return false;
+        if(i==0 && j==0) return 1;
+        if(i>0 && j==0) return 0;
+        if(i==0 && j>0){
+            for(int ii=1;ii<=j;ii++){
+                if(p[ii-1]!='*') return false;
             }
             return true;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?') return dp[i][j] = sol(i-1,j-1,s,p,dp);
-        if(p[j]=='*'){
+        if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j] = sol(i-1,j-1,s,p,dp);
+        if(p[j-1]=='*'){
             return dp[i][j] = sol(i-1,j,s,p,dp) | sol(i,j-1,s,p,dp);
         }
         return dp[i][j] = false;
+    }
+    bool solve(string&p ,int j){
+        for(int ii=1;ii<=j;ii++){
+                if(p[ii-1]!='*') return false;
+            }
+            return true;
     }
 public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-       vector<vector<int>> dp(n,vector<int>(m,-1));
-       return sol(n-1,m-1,s,p,dp);
+       vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+       dp[0][0]=1;
+       for(int i=1;i<=n;i++){
+        dp[i][0]=0;
+       }
+       for(int i=1;i<=m;i++){
+        dp[0][i] = solve(p,i);
+       }
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j] = dp[i-1][j-1];
+            else if(p[j-1]=='*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+            else dp[i][j] = 0;
+        }
+       }
+       return dp[n][m];
     }
 };
