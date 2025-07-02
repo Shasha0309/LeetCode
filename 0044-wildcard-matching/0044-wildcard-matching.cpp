c7ebe682ee1da82badd01ac31ce890bd4b33aeab
Shasha0309 <sona2002sharma@gmail.com>
2025-07-02 20:34:12 +0530

    Time: 28 ms (75.2%), Space: 14.8 MB (74.41%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,20 +1,5 @@
 class Solution {
-    bool sol(int i,int j,string& s,string& p,vector<vector<int>>& dp){
-        if(i==0 && j==0) return 1;
-        if(i>0 && j==0) return 0;
-        if(i==0 && j>0){
-            for(int ii=1;ii<=j;ii++){
-                if(p[ii-1]!='*') return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j] = sol(i-1,j-1,s,p,dp);
-        if(p[j-1]=='*'){
-            return dp[i][j] = sol(i-1,j,s,p,dp) | sol(i,j-1,s,p,dp);
-        }
-        return dp[i][j] = false;
-    }
+   
     bool solve(string&p ,int j){
         for(int ii=1;ii<=j;ii++){
                 if(p[ii-1]!='*') return false;
