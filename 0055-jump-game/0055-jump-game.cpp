005077445b228efbdcd95b731219b47efb98f713
Shasha0309 <sona2002sharma@gmail.com>
2025-07-11 20:52:04 +0530

    Time: 677 ms (6.14%), Space: 55.9 MB (6.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,21 @@
 class Solution {
+    bool sol(vector<int>& nums,int idx,vector<int>& dp){
+        if(idx==nums.size()-1) return true;
+        if(nums[idx]==0) return false;
+        if(dp[idx]!=-1) return dp[idx];
+        int reach = idx + nums[idx];
+        for(int i=idx+1;i<=reach;i++){
+            if(i<nums.size() && sol(nums,i,dp)) return dp[idx]=true;
+        } 
+        return dp[idx] = false;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = 0;
-        for(int i=0;i<n;i++){
-            if(maxi<i) return 0;
-            maxi = max(maxi,nums[i]+i);
-        }
-        return 1;
+        /*int n = nums.size();
+        vector<int> dp(n,0);
+        dp[n-1] = 1;
+        for(int i=n-2;i>=0;i--)*/
+        vector<int> dp(nums.size(),-1);
+        return sol(nums,0,dp);
     }
 };
