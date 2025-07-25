17075727cf4d7698a62191cae2deb871b6cbac3d
Shasha0309 <sona2002sharma@gmail.com>
2025-07-25 17:49:31 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,10 +4,9 @@ public:
         int ans = INT_MIN;
         int sum = 0;
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            ans = max(ans,sum);
-            if(sum<0) sum=0;
-            
+           sum+=nums[i];
+           if(sum>ans) ans=sum;
+           if(sum<0) sum=0;
         }
         return ans;
     }
