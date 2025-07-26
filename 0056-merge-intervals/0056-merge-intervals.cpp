36bdcf2716c9c7542e26f48b6bfe21ecf3f5cfe1
Shasha0309 <sona2002sharma@gmail.com>
2025-07-26 22:04:40 +0530

    Time: 7 ms (41.73%), Space: 23.7 MB (94.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,16 +4,14 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        
         for(int i=1;i<intervals.size();i++){
             if(ans.back()[1]>=intervals[i][0]){
                 if(ans.back()[1]>intervals[i][1]) continue;
-                ans.back()[1]=intervals[i][1];
-            }
+                ans.back()[1] = intervals[i][1];
+             }
             else{
                 ans.push_back(intervals[i]);
             }
-            
         }
         return ans;
     }
