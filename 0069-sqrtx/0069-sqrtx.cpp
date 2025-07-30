7ca2aecbe130495ec2b407ae47078d4e018994ad
Shasha0309 <sona2002sharma@gmail.com>
2025-07-31 01:50:25 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 || x==1) return x;
+      /*  if(x==0 || x==1) return x;
         int i=1;
         int j=x/2;
        // int ans = 0;
@@ -16,6 +16,15 @@ public:
                // ans=mid;
             }
         }
-        return j;
+        return j;*/
+         if (x == 0) return 0;
+    int left = 1, right = x;
+    while (left <= right) {
+        long long mid = left + (right - left) / 2;
+        if (mid * mid == x) return mid;
+        else if (mid * mid < x) left = mid + 1;
+        else right = mid - 1;
+    }
+    return right;
     }
 };
