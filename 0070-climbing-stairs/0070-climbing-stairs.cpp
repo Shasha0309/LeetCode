57949c870b51e2f64e5ae1e7e2ff428a93b23912
Shasha0309 <sona2002sharma@gmail.com>
2025-07-31 21:27:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,8 +6,14 @@ class Solution {
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        
-        return sol(n,dp);
+     //   vector<int> dp(n+1,-1);
+        int prev = 1;
+        int prev2 = 1;
+        for(int i=2;i<=n;i++){
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
