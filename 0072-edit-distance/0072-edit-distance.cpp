fbe0bc71b544825417476b1e9b48cd4fb43d0a52
Shasha0309 <sona2002sharma@gmail.com>
2025-08-01 20:35:47 +0530

    Time: 72 ms (5.46%), Space: 81.3 MB (12.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,7 +4,7 @@ class Solution {
         if(j<0) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
         if(w1[i]==w2[j]) return dp[i][j] = 0+sol(i-1,j-1,w1,w2,dp);
-        else return dp[i][j] = min(1+sol(i,j-1,w1,w2,dp),min(1+sol(i-1,j,w1,w2,dp),1+sol(i-1,j-1,w1,w2,dp)));
+        else return dp[i][j] = 1+ min(sol(i,j-1,w1,w2,dp),min(sol(i-1,j,w1,w2,dp),sol(i-1,j-1,w1,w2,dp)));
     }
 public:
     int minDistance(string word1, string word2) {
