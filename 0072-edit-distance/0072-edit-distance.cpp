816fb8e52d05975daad51b3849d03004d27a0189
Shasha0309 <sona2002sharma@gmail.com>
2025-08-02 02:11:52 +0530

    Time: 4 ms (80.3%), Space: 12.9 MB (74.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -11,6 +11,16 @@ public:
         int n = word1.length();
         int m = word2.length();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return sol(n,m,word1,word2,dp);
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int i=0;i<=m;i++) dp[0][i] = i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]=0+dp[i-1][j-1];
+                else{
+                    dp[i][j] = 1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
