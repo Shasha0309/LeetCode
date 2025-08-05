b10470c25704d4d31dc9c76acaa82d988f3718a1
Shasha0309 <sona2002sharma@gmail.com>
2025-08-05 20:27:19 +0530

    Time: 252 ms (10.87%), Space: 140 MB (8.19%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    void sol(int n,int ind,int ks,vector<vector<int>>& ans,vector<int> path){
+        
+        if(path.size()==ks){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=ind;i<=n;i++){
+        path.push_back(i);
+        sol(n,i+1,ks,ans,path);
+        path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        sol(n,1,k,ans,path);
+        return ans;
+    }
+};
