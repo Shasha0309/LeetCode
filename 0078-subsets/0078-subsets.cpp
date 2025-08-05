a8035dbae8562856b7f72f04d547611f419fe22a
Shasha0309 <sona2002sharma@gmail.com>
2025-08-06 01:57:30 +0530

    Time: 0 ms (100%), Space: 10.2 MB (37.33%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,16 +1,21 @@
 class Solution {
+    void sol(int ind,vector<int> nums,vector<vector<int>>& ans,vector<int>& path){
+        if(ind==nums.size()){
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            path.push_back(nums[i]);
+            ans.push_back(path);
+            sol(i+1,nums,ans,path);
+            path.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        int subset = 1<<n;
         vector<vector<int>> ans;
-        for(int i=0;i<subset;i++){
-            vector<int> res;
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))>0) res.push_back(nums[j]);
-            }
-            ans.push_back(res);
-        }
+        vector<int> path;
+        sol(0,nums,ans,path);
+        ans.push_back(path);
         return ans;
     }
 };
