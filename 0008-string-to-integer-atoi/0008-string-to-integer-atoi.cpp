b2988514dcacb556a6e7587ec7181eedb9b2781f
Shasha0309 <sona2002sharma@gmail.com>
2025-08-12 17:32:45 +0530

    Time: 0 ms (100%), Space: 9 MB (81.96%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        double num = 0;
         int i=0;
-        while(s[i]==' '){
-            i++;
-        }
-        bool pos = s[i] == '+' ;
-        bool neg = s[i] == '-' ;
-        pos == true ? i++ : i;
-        neg == true ? i++ : i;
+        while(s[i]==' ') i++;
+        bool pos = s[i] == '+';
+        bool neg = s[i] == '-';
+        pos == true ? i++:i;
+        neg == true ? i++:i;
+        double ans = 0;
         while(i<s.size() && s[i]>='0' && s[i]<='9'){
-            num = (num*10) + (s[i]-'0');
+            ans = (ans*10) + (s[i]-'0');
             i++;
         }
-        num = neg ? -num : num;
-        num = (num >INT_MAX) ? INT_MAX : num;
-        num = (num<INT_MIN) ? INT_MIN : num;
-        return int(num);
+        ans = neg ? -ans:ans;
+        ans = (ans>INT_MAX) ? INT_MAX : ans;
+        ans = (ans<INT_MIN) ? INT_MIN : ans;
+        return ans;
     }
 };
