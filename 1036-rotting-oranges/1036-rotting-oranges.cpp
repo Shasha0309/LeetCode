50547915eebc71572e2085344ded53a4c86ef8a3
Shasha0309 <sona2002sharma@gmail.com>
2025-08-15 01:04:38 +0530

    Time: 1 ms (40.42%), Space: 17.2 MB (19.19%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<pair<int,int>,int>> q;
-          vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>>vis(n,vector<int>(m,0));
         int total=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    vis[i][j]=2;
+                    vis[i][j]=1;
                     q.push({{i,j},0});
                 }
                 if(grid[i][j]==1){
@@ -19,19 +18,18 @@ public:
             }
         }
         int cnt = 0;
-        int tm = 0;
-      
-        int ro[] = {1,0,-1,0};
-        int co[] = {0,1,0,-1};
+        int t=0;
+        int row[] = {1,0,-1,0};
+        int col[] = {0,1,0,-1};
         while(!q.empty()){
             int x = q.front().first.first;
             int y = q.front().first.second;
             int tis = q.front().second;
             q.pop();
-            tm = max(tm,tis);
+            t=max(t,tis);
             for(int i=0;i<4;i++){
-                int drow = ro[i]+x;
-                int dcol = co[i]+y;
+                int drow = row[i]+x;
+                int dcol = col[i]+y;
                 if(drow>=0 && drow<n && dcol>=0 && dcol<m && grid[drow][dcol]==1 && vis[drow][dcol]!=2){
                     vis[drow][dcol] = 2;
                     q.push({{drow,dcol},tis+1});
@@ -40,6 +38,6 @@ public:
             }
         }
         if(cnt!=total) return -1;
-        return tm;
+        return t;
     }
 };
