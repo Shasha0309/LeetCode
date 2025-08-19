6c119a0c3298c61c37c56b87e6ee6ed54c09ec28
Shasha0309 <sona2002sharma@gmail.com>
2025-08-19 23:16:53 +0530

    Time: 11 ms (21.91%), Space: 20.1 MB (41.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
+        int n = numCourses;
+        vector<vector<int>> adj(n);
         for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
         }
-        vector<int> indeg(numCourses,0);
-        for(int i=0;i<numCourses;i++){
+        vector<int> indeg(n,0);
+        for(int i=0;i<n;i++){
             for(auto it:adj[i]){
-                indeg[it]++;
+            indeg[it]++;
             }
         }
+        int cnt=0;
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-           if(indeg[i]==0) q.push(i);
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0) q.push(i);
         }
-        vector<int>ans;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            ans.push_back(node);
+            cnt++;
             for(auto it:adj[node]){
                 indeg[it]--;
                 if(indeg[it]==0) q.push(it);
             }
         }
-        if(ans.size()==numCourses) return true;
+        if(cnt==n) return true;
         return false;
     }
 };
