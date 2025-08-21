eda78ee82e0bdb965e443cdea07111f2350340ad
Shasha0309 <sona2002sharma@gmail.com>
2025-08-21 16:28:59 +0530

    Time: 82 ms (27.72%), Space: 24.4 MB (75.59%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -3,31 +3,33 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        dist[0][0]=0;
-        int drow[4] = {0,1,0,-1};
-        int dcol[4] = {1,0,-1,0};
-        while(!pq.empty()){
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+        pq.push({0, {0, 0}});
+        vector<vector<int>> dif(n, vector<int>(m, 1e9));
+        dif[0][0] = 0;
+        int row[] = {-1, 0, +1, 0};
+        int col[] = {0, +1, 0, -1};
+        while (!pq.empty()) {
             auto it = pq.top();
-            int r = it.second.first;;
-            int c = it.second.second;
-            int dis = it.first;
             pq.pop();
-            if(r==n-1 && c==m-1) return dis;
-            for(int i=0;i<4;i++){
-                int nr = r+drow[i];
-                int nc = c+dcol[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int newdif = max(abs(heights[r][c]-heights[nr][nc]),dis);
-                    if(newdif<dist[nr][nc]){
-                        pq.push({newdif,{nr,nc}});
-                        dist[nr][nc]=newdif;
-                    }
+            int currDif = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
+            if (i == n - 1 && j == m - 1)
+                return currDif; // already reached
+            for (int k = 0; k < 4; k++) {
+                int r = i + row[k];
+                int c = j + col[k];
+                if ((r >= 0 && r <= n - 1 && c <= m - 1 && c >= 0) &&
+                    abs(heights[i][j] - heights[r][c]) < dif[r][c]) {
+                    dif[r][c] = abs(heights[i][j] - heights[r][c]);
+                    pq.push({max(currDif, dif[r][c]), {r, c}});
                 }
             }
         }
-        return 0;
+        return dif[n - 1][m - 1];
     }
 };
